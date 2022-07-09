d1f8c6747a95250d73ba8ffe7a9d8a293cfb4738
Маис <Kozannostra777@mail.ru>
2022-07-10 00:06:41 +0300

    класс новыйМяу

---
+++ sem2animals/newMeow.java
@@ -0,0 +1,12 @@
+public class newMeow implements Voice{
+
+    @Override
+    public void voice(){
+        nMeow();        
+    }
+
+    public void nMeow(){
+        System.out.println("Мяяяяяяу Мяяяяяяяяяяяяу");
+    }
+    
+}
