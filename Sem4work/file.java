bc92d9a2ff8849869ca5bce76e53c48d85bf0a24
Маис <Kozannostra777@mail.ru>
2022-07-31 18:36:58 +0300

    1236363

---
+++ Sem4work/file.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class file {
+
+    String name;
+    List<String> file;
+
+
+    public file(String name) {
+        this.name = name;
+        file = new ArrayList<>();
+    }
+    
+}
---
+++ Sem4work/save.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class save<E> extends file{
+    private List<E> fileS;
+    String formatS;
+    
+    public save(String name) {
+        super(name);
+        //TODO Auto-generated constructor stub
+    }
+
+    // List<E> newFile =new ArrayList<E>();
+
+    
+
+    public void xml("принимает файл"){
+        // логика
+        // дает на выходе файл в xml
+
+    }
+
+    public void word(){
+        // 
+    }
+
+    public void pdf(){
+        // 
+    }
+
+
+
+
+    
+}
